c454dab0cd1017137ecf87844c32a5b35a81e013
Jipu Wang <jipuwang@umich.edu>
2017-07-12 16:40:40 -0400

    adapt manufacturer for 1d2 angles purposes, the integration from angular flux to scalar flux has to be done manufally

--- manufacturer_1d2angle.m
+++ manufacturer_1d2angle.m
@@ -2,15 +2,17 @@
     % Discretized analytical solution
     % Manufactured boundary conditions
     % Manufactured source
-function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
-          T_MMS_j,T_L,T_R,q_MMS_j]=...
-          manufacturer(J,N,Tau,mat,assumedSoln,fbType)
+function [phi0_MMS_j,psi_b1_n_i,psi_b2_n_i,Q_MMS_j_n_i]=...
+          manufacturer_1d2angle(J,N,I,Tau,mat,assumedSoln)
   % input parameters
   if ~exist('J','var')
     J=5*2;%*2%*2*2*2*2*2*2*2*2
   end
   if ~exist('N','var')
     N=16;
+  end
+  if ~exist('I','var')
+    I=16;
   end
   if ~exist('Tau','var')
     Tau=10;
@@ -28,13 +30,9 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
       field4,value4,field5,value5,field6,value6,field7,value7);
   end
   if ~exist('assumedSoln','var')
-    assumedSoln='sine_sine';
+    assumedSoln='sine_sine_sine';
   end
-  if ~exist('fbType','var')
-    fbType='linear';
-%     fbType='noFeedback';
-%     fbType='sqareRoot';
-  end
+  
   % Material
   Sig_t_j=mat.Sig_t_j;
   Sig_ss_j=mat.Sig_ss_j;
@@ -46,98 +44,69 @@ function [phi0_MMS_j,psi_b1_n,psi_b2_n,Q_MMS_j_n,...
 
   h=Tau/J;
   [mu_n,weight_n]=lgwt(N,-1,1); mu_n=flipud(mu_n);
-  
+  [alpha_i,weight_i]=lgwt(I,0,2*pi);alpha_i=flipud(alpha_i);
+
   %% Manufactured Solutions for both fields
   % They need to be pre-defined here due to temperature dependence on the
   % xs. 
   % Options includes: sine_sine, const_cubic, sqrtPlus1_quadratic, etc.
   switch(assumedSoln)
-    case('sine_sine')
+    case('sine_sine_sine')
       % Manufactured neutronics solution \psi(x,\mu)=sin(pi*x/Tau), 0<x<Tau
-      psi_MMS =@(x) sin(pi*x/Tau);
-      psi_MMS_Diff =@(x) pi/Tau*cos(pi*x/Tau);
-      % Manufactured TH solution T(x)=sin(pi*x/Tau), 0<x<Tau
-      T_MMS =@(x) sin(pi*x/Tau);
-      T_MMS_xx =@(x) -(pi*pi/Tau/Tau)*sin(pi*x/Tau);
+      psi_MMS =@(x,mu,alpha) (sin(pi/Tau*x)+1).*sin(mu+1).*sin(alpha/(2*pi));
+      psi_MMS_Diff =@(x,mu,alpha) pi/Tau*cos(pi/Tau*x).*sin(mu+1).*sin(alpha/(2*pi));
     case('const_cubic')
-      % Manufactured neutronics solution \psi(x,\mu)=1.0, 0<x<Tau
-      psi_MMS =@(x) 1.0+x*0.0;
-      psi_MMS_Diff =@(x) x*0.0;
-      % Manufactured TH solution T(x)=x.^3, 0<x<Tau
-      T_MMS =@(x) x.^3;
-      T_MMS_xx =@(x) x*6.0;
     case('sqrtPlus1_quadratic')
-      % Manufactured neutronics solution \psi(x,\mu)=1.0, 0<x<Tau
-      psi_MMS =@(x) sqrt(x+1);
-      psi_MMS_Diff =@(x) 0.5./sqrt(x+1);
-      % Manufactured TH solution T(x)=x.^2, 0<x<Tau
-      T_MMS =@(x) x.^2;
-      T_MMS_xx =@(x) 2.0+x*0.0;
-  end
-  
-  %% XS update due to temperature feedback!
-  % Change in capture is reflected in change in total. 
-  switch fbType
-    case 'noFeedback'
-      Sig_gamma =@(x) mat.Sig_gamma_j(1)+0.0*x;
-    case 'linear'
-      % Assumes the original xs is homogeneous
-      T0=50;
-      gamma_coeff=0.004;
-      Sig_gamma =@(x) mat.Sig_gamma_j(1)+gamma_coeff*(T_MMS(x)-T0);
-    case 'squareRootPlus1'
-      T0=50;
-      Sig_gamma =@(x) mat.Sig_gamma_j(1)*sqrt((T0+1)./(T_MMS(x)+1));
   end
   
   Sig_ss =@(x) Sig_ss_j(1)+x*0;
+  Sig_gamma =@(x) mat.Sig_gamma_j(1)+0.0*x;
   Sig_f =@(x) Sig_f_j(1)+x*0;
   nuSig_f =@(x) nuSig_f_j(1)+x*0;
   Sig_t =@(x) Sig_ss(x)+Sig_f(x)+Sig_gamma(x);
   
-  phi0_MMS =@(x) 2*psi_MMS(x);
+  % phi0_MMS =@(x) integral2(@(mu,alpha) psi_MMS(x,mu,alpha), -1,1, 0,2*pi);
+  % The above function handle can support fplot, and evaluation, but not
+  % further integral.  So I have to do it by hand. 
+  phi0_MMS =@(x) (sin(pi/Tau*x)+1)*(1-cos(2))*(-2*pi*(cos(1)-1));
   % MMS source: mu_n * derivative(psi_MMS) +Sig_t* psi_MMS ...
   % -(Sig_ss+nuSig_f)*0.5*phi0_MMS;
-  Q_MMS =@(x,mu) mu*psi_MMS_Diff(x) +Sig_t(x).*psi_MMS(x) ...
-    -(Sig_ss(x)+nuSig_f(x))*0.5.*phi0_MMS(x);
+  Q_MMS =@(x,mu,alpha) mu*psi_MMS_Diff(x,mu,alpha) ...
+    +Sig_t(x).*psi_MMS(x,mu,alpha) ...
+    -1/(4*pi)*Sig_ss(x).*phi0_MMS(x);
+  % no fission source
+%     -(Sig_ss(x)+nuSig_f(x))/(4*pi).*phi0_MMS(x);
   
   %% For MoC MMS solution and problem
   % Boundary condition and source
+  psi_b1_n_i=zeros(N,I);
   % psi expression evaluated at x=0
-  psi_b1_n=psi_MMS(0)*ones(N,1); % n=N/2+1:N % mu>0
+  for n=1:N
+    for i=1:I
+      psi_b1_n_i(n,i)=psi_MMS(0,mu_n(n),alpha_i(i)); % n=N/2+1:N % mu>0
+    end
+  end
   % psi expression evaluated at x=Tau
-  psi_b2_n=psi_MMS(Tau)*ones(N,1); % n=1:N/2 % mu<0
-
+  psi_b2_n_i=zeros(N,I);
+  for n=1:N
+    for i=1:I
+      psi_b2_n_i(n,i)=psi_MMS(Tau,mu_n(n),alpha_i(i)); % n=N/2+1:N % mu>0
+    end
+  end
+  
   phi0_MMS_j=zeros(J,1);
-  Q_MMS_j_n=zeros(J,N); % preallocate memory, avg'ed over tau_(j-1/2) and tau_(j+1/2)
+  Q_MMS_j_n_i=zeros(J,N,I);
 
   for j=1:J
     x_L=(j-1)*h;x_R=j*h;
     phi0_MMS_j(j)=1/h*integral(phi0_MMS,x_L,x_R);
     for n=1:N
-      % g = @(c) (integral(@(x) (x.^2 + c*x + 1),0,1));
-      Q_MMS_j_n(j,n)=integral(@(x) Q_MMS(x,mu_n(n)),x_L,x_R)/h;
+      for i=1:I
+        % g = @(c) (integral(@(x) (x.^2 + c*x + 1),0,1));
+        Q_MMS_j_n_i(j,n,i)= ...
+          integral(@(x) Q_MMS(x,mu_n(n),alpha_i(i)),x_L,x_R)/h;
+      end % i
     end % n
   end % j
   
-  %% For TH MMS solution and problem
-  % Boundary condition and source
-  % Left boundary, T_MMS evaluated at x=0;
-  T_L=T_MMS(0);
-  % Right boundary, T_MMS evaluated at x=Tau;
-  T_R=T_MMS(Tau);
-  
-  % Discretized MMS solution
-  T_MMS_j=zeros(J,1);
-  T_MMS_xx_j=zeros(J,1);
-
-  % MMS source
-  q_MMS_j=zeros(J,1);
-  for j=1:J
-    x_L=(j-1)*h;x_R=j*h;
-    T_MMS_j(j)=1/h*integral(T_MMS,x_L,x_R);
-    T_MMS_xx_j(j)=1/h*integral(T_MMS_xx,x_L,x_R);
-    q_MMS_j(j)=k_F*T_MMS_xx_j(j)+kappaSig_f_j(j)*phi0_MMS_j(j);
-  end  
-
 end
