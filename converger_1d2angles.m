637db143cfa9119e970f0d010b8656ad70939758
Jipu Wang <jipuwang@umich.edu>
2017-07-13 10:10:10 -0400

    modified converger for 1d 2angles purposes

--- converger_1d2angles.m
+++ converger_1d2angles.m
@@ -9,26 +9,24 @@
 % the problem description. 
 % It needs to know the geometry and is responsible for generating the grid
 % and pass the grid information to the coupler. 
-function [order_phi,order_T]=converger(fbType,mocSrc,assumedSoln)
+function [order_phi]=converger_1d2angles(assumedSoln)
 % clear;
-nGrids=6;%10;%8;
+nGrids=2%6;%10;%8;
 refinementRatio=2;
 
 % Geometry
 Tau=10; 
 
-% % Case configure options
-% % fbType options: 'noFeedback','linear','squareRootPlus1'
-% fbType='noFeedback'; 
-% % mocSrc options: 'flat_source','linear_source'
-% mocSrc='flat_source';
-% % assumedSoln options: 'const_quadratic','sine_sine','sqrtPlus1_quadratic"
-% assumedSoln='sqrtPlus1_quadratic'; 
+% Case configure options
+if ~exist('assumedSoln','var')
+  assumedSoln='sine_sine_sine';
+end
 
 error_phi0_n=zeros(nGrids,1);
-error_T_n=zeros(nGrids,1);
 gridMeshSize_n=ones(nGrids,1);
 N=16; % angular discretization, fixed not refined. 
+I=16;
+
 for iGrid=1:nGrids
   J=5*refinementRatio^iGrid;
   gridMeshSize_n(iGrid)=Tau/J;
@@ -44,60 +42,21 @@ for iGrid=1:nGrids
   mat = struct(field1,value1,field2,value2,field3,value3,... 
     field4,value4,field5,value5,field6,value6,field7,value7);
 
-  % Define function handles 
-  if strcmp(mocSrc,'linear_source')
-    % call the manufacturer to get MMS problem and solution
-    [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n,Q_MMS_hat_j_n, ...
-          T_j_ana,T_L,T_R,p_MMS_j]=...
-          manufacturer_LS(J,N,Tau,mat,assumedSoln,fbType);
+  [phi0_j_ana,psi_b1_n_i,psi_b2_n_i,Q_MMS_j_n_i]=...
+        manufacturer_1d2angles(J,N,I,Tau,mat,assumedSoln);
+      
+  [phi0_j]=OneDMoC_2Angles(J,N,I,Tau,mat,...
+    psi_b1_n_i,psi_b2_n_i,Q_MMS_j_n_i);
 
-    % call the coupler to solve the above manufactured problem
-    [phi0_j,T_j]=coupler_LS(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n,Q_MMS_hat_j_n, ...
-                T_L,T_R,p_MMS_j,fbType, ...
-              phi0_j_ana,T_j_ana);
-  else
-    [phi0_j_ana,psi_b1_n,psi_b2_n,Q_MMS_j_n, ...
-          T_j_ana,T_L,T_R,p_MMS_j]=...
-          manufacturer(J,N,Tau,mat,assumedSoln,fbType);
-
-    % call the coupler to solve the above manufactured problem
-    [phi0_j,T_j]=coupler(J,N,Tau,mat,psi_b1_n,psi_b2_n,Q_MMS_j_n, ...
-                T_L,T_R,p_MMS_j,fbType);
-  end
-
-  
   % Calculate the error compared to manufactured solution
   error_phi0_n(iGrid)=norm(phi0_j-phi0_j_ana,2)/sqrt(J);
-  error_T_n(iGrid)=norm(T_j-T_j_ana,2)/sqrt(J);
   
-%   %% Plot the solution over grid refinements
-%   x=linspace(0,10,J);
-%   figure(67); clf; hold on;
-%   if iGrid==1
-%     % title('scalar flux');
-%     xlabel('mesh size [cm]');
-%     ylabel('scalar flux');
-%   end
-%   % Plot the solution
-%   plot(x,phi0_j,'-*');
-%   
-%   figure(68); clf; hold on;
-%   if iGrid==1
-%     % title('temperature');
-%     xlabel('mesh size [cm]');
-%     ylabel('temperature');
-%   end
-%   plot(x,T_j,'-o');
-
 end
 
 % Calculate the order of accuracy
 order_phi_nMinus1=ones(nGrids-1,1);
-order_T_nMinus1=ones(nGrids-1,1);
 for j=1:nGrids-1
   order_phi_nMinus1(j)=log(error_phi0_n(j)/error_phi0_n(j+1)) / ...
-    log(gridMeshSize_n(j)/gridMeshSize_n(j+1));
-  order_T_nMinus1(j)=log(error_T_n(j)/error_T_n(j+1)) / ...
     log(gridMeshSize_n(j)/gridMeshSize_n(j+1));
 end
 
@@ -125,40 +84,14 @@ end
 %   '3rd Order','4th Order','location','best');
 % hold off;
 % 
-% temperatureErrorRM_plot_handle=figure(12);
-% loglog(gridMeshSize_n,error_T_n,'*');
-% % title('temperature error convergence');
-% xlabel('mesh size [cm]');
-% ylabel('temperature error RMS');
-% 
-% hold on;
-% orderGuess=round(order_T_nMinus1(end));
-% errorStt=error_T_n(end)*refinementRatio^(orderGuess*(nGrids-1));
-% firstOrder=[errorStt errorStt/refinementRatio^(nGrids-1)];
-% secondOrder=[errorStt errorStt/refinementRatio^(2*(nGrids-1))];
-% thirdOrder=[errorStt errorStt/refinementRatio^(3*(nGrids-1))];
-% fourthOrder=[errorStt errorStt/refinementRatio^(4*(nGrids-1))];
-% loglog(orderPlotGrid,firstOrder,'--');
-% loglog(orderPlotGrid,secondOrder,'--');
-% loglog(orderPlotGrid,thirdOrder,'--');
-% loglog(orderPlotGrid,fourthOrder,'--');
-% legend('temperature error','1st Order','2nd Order',...
-%   '3rd Order','4th Order','location','best');
-% hold off;
-% 
+
 % % Plot the solution
-% scalarFlux_plot_handle=figure(13);r
+% scalarFlux_plot_handle=figure(13);
 % plot(phi0_j,'-*');
 % % title('scalar flux');
 % xlabel('mesh size [cm]');
 % ylabel('scalar flux');
-% 
-% temperature_plot_handle=figure(14);
-% plot(T_j,'-o');
-% % title('temperature');
-% xlabel('mesh size [cm]');
-% ylabel('temperature');
-% 
+
 % % Save the plots
 % phi0_RMS_fn=char(strcat('fbType_',fbType,'_mocSrc_',mocSrc,'_soln_',assumedSoln,'_','phi0_RMS'));
 % T_RMS_fn=char(strcat('fbType_',fbType,'_mocSrc_',mocSrc,'_soln_',assumedSoln,'_','T_RMS'));
@@ -172,21 +105,14 @@ end
 % Display the problem description
 
 disp '=================';
-% % display(fbType);
-% % display(mocSrc)
-% % display(assumedSoln);
+display(assumedSoln);
 % % Display the result
 error_phi0_n
-error_T_n
 order_phi_nMinus1
-order_T_nMinus1
-display(char(strcat('fbType_',fbType,'_mocSrc_',mocSrc,'_soln_',assumedSoln)));
+display(char(strcat('soln_',assumedSoln)));
 display(char(num2str(order_phi_nMinus1(end))));
-display(char(num2str(order_T_nMinus1(end))));
-
 
 order_phi=order_phi_nMinus1(end);
-order_T=order_T_nMinus1(end);
 
 % aa=0.0;
 end
